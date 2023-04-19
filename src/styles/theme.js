99258f9f9363d566ad6c71ce9e4ad11179b6262d
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 00:29:58 +0300

    Update theme.js

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -6,29 +6,17 @@ export const theme = {
     buttonColor: '#2196f3',
     accentColor: '#2196f3',
     iconColor: '#afb1b8',
+    borderColorHeader: '#ececec',
+    bgColorBurgerMenu: '#ececec',
+    bgColorFooter: '#2f303a',
+    darkColor: '#212121',
   },
 
   spacing: n => `${4 * n}px`,
 
   media: {
     medium: '(min-width: 768px)',
-    large: '(min-width: 1200px)',
-  },
-
-  fontSizes: {
-    xxs: '8px',
-    x: '10px',
-    xs: '12px',
-    s: '14px',
-    m: '16px',
-    xm: '18px',
-    l: '24px',
-    xl: '28px',
-    xxl: '32px',
-    sb: '44px',
-    mb: '60px',
-    lb: '72px',
-    b: '100px',
+    large: '(min-width: 1140px)',
   },
 
   fontWeights: {
@@ -36,6 +24,22 @@ export const theme = {
     medium: 500,
     semiBold: 600,
     bold: 700,
+  },
+
+  fontSizes: {
+    xxs: '8px',
+    x: '10px',
+    xs: '12px',
+    s: '14px',
+    m: '16px', //
+    xm: '18px', //
+    l: '24px', //
+    xl: '26px', //
+    xxl: '32px', //
+    sb: '44px',
+    mb: '60px',
+    lb: '72px',
+    b: '100px',
   },
 
   borders: {
@@ -51,4 +55,8 @@ export const theme = {
   },
 
   shadows: {},
+
+  transitions: {
+    main: 'all 300ms cubic-bezier(0.4, 0, 0.2, 1)',
+  },
 };
