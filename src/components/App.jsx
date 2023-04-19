aa7f6e9e05cf3bf64eead259be3c39eccfa8d6a5
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 00:35:25 +0300

    Update App.jsx
    add Routes UsersPage PersonPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout';
 
 const MainPage = lazy(() => import('../pages/Main'));
+const UsersPage = lazy(() => import('../pages/Users'));
+const PersonPage = lazy(() => import('../pages/Person'));
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<MainPage />} />
+        <Route path="users" element={<UsersPage />} />
+        <Route path="person" element={<PersonPage />} />
       </Route>
     </Routes>
   );
