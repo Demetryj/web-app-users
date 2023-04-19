b5dbda916f4b6c456d496cbbb484f101567452b7
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 00:24:47 +0300

    Update App.jsx
    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,15 @@
-// import { Routes, Route } from 'react-router-dom';
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { SharedLayout } from './SharedLayout';
+
+const MainPage = lazy(() => import('../pages/Main'));
 
 export const App = () => {
-  return <div>Web-app-users</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<MainPage />} />
+      </Route>
+    </Routes>
+  );
 };
