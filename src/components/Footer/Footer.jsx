818d3999be46fc730a3ea34cd8e269882e2d692c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 00:25:12 +0300

    Create Footer.jsx

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import { FooterWrapper, TextWrapper, Text, FooterIcon } from './Footer.styled';
+
+export const Footer = () => {
+  return (
+    <FooterWrapper>
+      <TextWrapper>
+        <FooterIcon />
+        <Text>2023 WepApplication.</Text>
+      </TextWrapper>
+    </FooterWrapper>
+  );
+};
