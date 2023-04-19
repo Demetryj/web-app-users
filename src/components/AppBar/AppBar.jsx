77920e3820aa0a8ab612a70f5657904d0abd1cc7
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 00:24:55 +0300

    Create AppBar.jsx

---
+++ src/components/AppBar/AppBar.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { NavLinkList } from '../NavLinkList';
+import { useMedia } from '../../hooks/useMedia';
+import { BurgerMenu } from '../BurgerMenu';
+
+import {
+  Header,
+  Logo,
+  ButtonMenu,
+  OpenMenuIcon,
+  CloseMenuIcon,
+  Border,
+} from './AppBar.styled';
+
+export const AppBar = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const { isMobile } = useMedia();
+  let visible;
+
+  if (isMobile) {
+    visible = true;
+  } else {
+    visible = false;
+  }
+
+  const toggleMenu = () => {
+    setIsOpenMenu(isOpenMenu => !isOpenMenu);
+  };
+
+  return (
+    <Header>
+      <Logo to="/">WebAppUsers</Logo>
+      {!visible && (
+        <nav>
+          <NavLinkList />
+        </nav>
+      )}
+
+      {isOpenMenu && <BurgerMenu onClick={toggleMenu} />}
+
+      <ButtonMenu isVisible={visible} onClick={toggleMenu}>
+        {!isOpenMenu ? <OpenMenuIcon /> : <CloseMenuIcon />}
+      </ButtonMenu>
+
+      <Border></Border>
+    </Header>
+  );
+};
