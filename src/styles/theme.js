40971956b709d75d3b57be38bf235e54eabacbfc
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-19 12:28:41 +0300

    Create theme.js

---
+++ src/styles/theme.js
@@ -0,0 +1,54 @@
+export const theme = {
+  colors: {
+    primaryFontColor: '#212121',
+    secondaryFontColor: '#757575',
+    witeTextColor: '#ffffff',
+    buttonColor: '#2196f3',
+    accentColor: '#2196f3',
+    iconColor: '#afb1b8',
+  },
+
+  spacing: n => `${4 * n}px`,
+
+  media: {
+    medium: '(min-width: 768px)',
+    large: '(min-width: 1200px)',
+  },
+
+  fontSizes: {
+    xxs: '8px',
+    x: '10px',
+    xs: '12px',
+    s: '14px',
+    m: '16px',
+    xm: '18px',
+    l: '24px',
+    xl: '28px',
+    xxl: '32px',
+    sb: '44px',
+    mb: '60px',
+    lb: '72px',
+    b: '100px',
+  },
+
+  fontWeights: {
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+  },
+
+  borders: {
+    none: 'none',
+    normal: '1px solid',
+    medium: '2px solid',
+  },
+
+  radii: {
+    none: '0',
+    superLight: '4px',
+    light: '6px',
+  },
+
+  shadows: {},
+};
