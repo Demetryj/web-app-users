f8dc95a59156d0a33422a59683b37d0b7dc79905
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 00:25:30 +0300

    Create SharedLayout.jsx

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,19 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { AppBar } from '../AppBar';
+import { Container } from './SharedLayout.styled';
+// import { Footer } from '../Footer';
+
+export const SharedLayout = () => {
+  return (
+    <Container>
+      <AppBar />
+      <main>
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </main>
+      {/* <Footer /> */}
+    </Container>
+  );
+};
