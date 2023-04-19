354f029d82e822297f2dce7cea90d648cdd40356
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-19 12:28:32 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
-import { App } from './App';
+// import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
+// import { store } from './redux/store';
+import { theme, GlobalStyles } from './styles';
+import { App } from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
+    {/* <Provider store={store}> */}
+    <ThemeProvider theme={theme}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+        <GlobalStyles />
+      </BrowserRouter>
+    </ThemeProvider>
+    {/* </Provider> */}
   </React.StrictMode>
 );
