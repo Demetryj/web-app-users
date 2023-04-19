32cf7577cc771e56eaea2f550e7c667580b45f94
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-19 12:29:06 +0300

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+// import { configureStore } from '@reduxjs/toolkit';
+// import { usersReducer } from './users/usersSlice';
+// import { filterReducer } from './filter/feilterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     users: usersReducer,
+//     filter: filterReducer,
+//   },
+// });
