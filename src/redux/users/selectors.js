c3152687c04676e32ccc6b412825b9849a373131
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:38:11 +0300

    Update selectors.js

--- src/redux/users/selectors.js
+++ src/redux/users/selectors.js
@@ -3,6 +3,8 @@ import { selectFilterValue } from '../filter/selectors';
 
 export const selectUsers = state => state.users.items;
 
+export const selectPerson = state => state.users.person;
+
 export const selectIsLoading = state => state.users.isLoading;
 
 export const selectError = state => state.users.error;
@@ -12,8 +14,10 @@ export const selectVisibleUsers = createSelector(
   (users, filterValue) => {
     const normalyzeFilter = filterValue.toLowerCase();
 
-    const visibleUsers = users.filter(user =>
-      user.name.toLowerCase().includes(normalyzeFilter)
+    const visibleUsers = users.filter(
+      user =>
+        user.name.toLowerCase().includes(normalyzeFilter) ||
+        user.username.toLowerCase().includes(normalyzeFilter)
     );
 
     return visibleUsers;
