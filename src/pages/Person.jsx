11c78fd7ce3198b206376811c7e039d9a7eb5449
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:37:55 +0300

    Update Person.jsx

--- src/pages/Person.jsx
+++ src/pages/Person.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { getUserById } from '../redux/users/operations';
+
+import { UserData } from '../components/User/User';
+
 const Person = () => {
-  return <div>Person page</div>;
+  const dispatch = useDispatch();
+  const { userId } = useParams();
+
+  useEffect(() => {
+    dispatch(getUserById(userId));
+  }, [dispatch, userId]);
+
+  return (
+    <section>
+      <UserData />
+    </section>
+  );
 };
 
 export default Person;
