e8fac07c5abbc5b43d6d8cc21384185ad9d0e975
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:37:34 +0300

    Update NavLinkList.jsx

--- src/components/NavLinkList/NavLinkList.jsx
+++ src/components/NavLinkList/NavLinkList.jsx
@@ -1,16 +1,16 @@
 import { NavList, NavItem } from './NavLinkList.styled';
 
 const navRoutes = [
-  { item: 'Users', route: '/users' },
-  { item: 'Person', route: 'person' },
+  { item: 'Users', route: 'users' },
+  { item: 'Person', route: 'users/:userId' },
 ];
 
-export const NavLinkList = ({ mobile }) => {
+export const NavLinkList = ({ mobile, onCloseMenu }) => {
   return (
     <NavList mobile={mobile}>
       {navRoutes.map(({ item, route }) => {
         return (
-          <li key={item}>
+          <li key={item} onClick={onCloseMenu}>
             <NavItem to={route} mobile={mobile}>
               {item}
             </NavItem>
