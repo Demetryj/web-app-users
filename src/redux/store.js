77410b13770ed7c8ab9927a73458bb3753f23bed
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-21 00:30:36 +0300

    Update store.js
    add Persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { usersReducer } from './users/usersSlice';
 import { filterReducer } from './filter/feilterSlice';
 
+const usersPersistConfig = {
+  key: 'users',
+  storage,
+};
+
 export const store = configureStore({
   reducer: {
-    users: usersReducer,
+    users: persistReducer(usersPersistConfig, usersReducer),
     filter: filterReducer,
   },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  ],
 });
+
+export const persistor = persistStore(store);
