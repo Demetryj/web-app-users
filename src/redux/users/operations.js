ff4eb970bf4a73a854c619d892fc645d47ff2b87
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:38:02 +0300

    Update operations.js

--- src/redux/users/operations.js
+++ src/redux/users/operations.js
@@ -1,13 +1,25 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com';
 
 export const getUsers = createAsyncThunk(
   'users/getUsers',
   async (_, thunkAPI) => {
     try {
       const response = await axios.get('/users');
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const getUserById = createAsyncThunk(
+  'users/getUserById',
+  async (userId, thunkAPI) => {
+    try {
+      const response = await axios.get(`/users/${userId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -29,9 +41,9 @@ export const addUser = createAsyncThunk(
 
 export const deleteUser = createAsyncThunk(
   'users/deleteUser',
-  async (usersId, thunkAPI) => {
+  async (userId, thunkAPI) => {
     try {
-      const response = await axios.delete(`users/${usersId}`);
+      const response = await axios.delete(`users/${userId}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -41,9 +53,9 @@ export const deleteUser = createAsyncThunk(
 
 export const updateUser = createAsyncThunk(
   'users/updateUser',
-  async ({ usersId, name, username, email }, thunkAPI) => {
+  async ({ userId, name, username, email }, thunkAPI) => {
     try {
-      const response = await axios.patch(`users/${usersId}`, {
+      const response = await axios.patch(`users/${userId}`, {
         name,
         username,
         email,
