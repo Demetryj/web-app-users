2c75a01e9b4228c190db51f9c2058045f472e04a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:37:58 +0300

    Update Users.jsx

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { DataUsers } from '../components/DataUsers/DataUsers';
+import { SearchUser } from '../components/SearchUser/SearchUser';
+import { getUsers } from '../redux/users/operations';
+
 const Users = () => {
-  return <div>Users page</div>;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
+  return (
+    <section>
+      <SearchUser />
+      <DataUsers />
+    </section>
+  );
 };
 
 export default Users;
