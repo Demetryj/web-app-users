afd8e2eae5bc762f9426cf859fdce174c4ae455f
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:38:27 +0300

    Create SearchUser.styled.js

---
+++ src/components/SearchUser/SearchUser.styled.js
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+
+export const WrapperFilter = styled.div`
+  margin: 0 auto ${props => props.theme.spacing(6)};
+  /* margin-bottom: ${props => props.theme.spacing(6)}; */
+  padding: ${props => props.theme.spacing(2)};
+  width: 300px;
+
+  border: ${props => props.theme.borders.normal};
+  border-radius: ${props => props.theme.radii.light};
+  border-color: ${props => props.theme.colors.borderColor};
+  box-shadow: ${props => props.theme.shadows.second};
+`;
+
+export const Label = styled.label`
+  display: flex;
+  flex-direction: column;
+  gap: ${props => props.theme.spacing(1)};
+
+  font-size: ${props => props.theme.fontSizes.m};
+  font-weight: ${props => props.theme.fontWeights.bold};
+  color: ${props => props.theme.colors.primaryFontColor};
+
+  &:focus-within {
+    color: ${props => props.theme.colors.accentColor};
+  }
+`;
+
+export const Input = styled.input`
+  padding: ${props => props.theme.spacing(1)};
+
+  font-family: inherit;
+  font-weight: ${props => props.theme.fontWeights.regular};
+  font-size: ${props => props.theme.fontSizes.m};
+
+  border-radius: ${props => props.theme.radii.light};
+
+  &:placeholder-shown {
+    border-color: ${props => props.theme.colors.borderColor};
+  }
+
+  &:not(:placeholder-shown) {
+    border-color: ${props => props.theme.colors.accentColor};
+  }
+`;
