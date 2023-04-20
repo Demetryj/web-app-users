476f4af8143a9a80e83c8d63e40dc1822d82ed8f
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-21 00:30:11 +0300

    Update index.js
    add PersistGate

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { ThemeProvider } from 'styled-components';
-import { store } from './redux/store';
+import { store, persistor } from './redux/store';
 import { theme, GlobalStyles } from './styles';
 import { App } from './components/App';
 
@@ -11,12 +12,14 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <App />
-          <GlobalStyles />
-        </BrowserRouter>
-      </ThemeProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ThemeProvider theme={theme}>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <App />
+            <GlobalStyles />
+          </BrowserRouter>
+        </ThemeProvider>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
