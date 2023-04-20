bad593cf8c7b653c52131423e2fc9d17ac4debb3
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 22:38:14 +0300

    Update useUsers.js

--- src/hooks/useUsers.js
+++ src/hooks/useUsers.js
@@ -1,15 +1,23 @@
 import { useSelector } from 'react-redux';
 import {
   selectUsers,
+  selectPerson,
   selectIsLoading,
   selectError,
   selectVisibleUsers,
 } from '../redux/users/selectors';
 
-export const useContacts = () => {
+export const useUsers = () => {
   const users = useSelector(selectUsers);
+  const person = useSelector(selectPerson);
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
   const visibleUsers = useSelector(selectVisibleUsers);
-  return { users, isLoading, error, visibleUsers };
+  return {
+    users,
+    person,
+    isLoading,
+    error,
+    visibleUsers,
+  };
 };
