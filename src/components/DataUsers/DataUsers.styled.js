0c82d1ba9bacd75f5f934e1437faceb352fd6489
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 13:58:00 +0300

    Create DataUsers.styled.js

---
+++ src/components/DataUsers/DataUsers.styled.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  margin-right: auto;
+  margin-left: auto;
+  border-spacing: ${p => p.theme.spacing(0)};
+  box-shadow: ${p => p.theme.shadows.table};
+`;
+
+export const HeadCell = styled.th`
+  font-size: ${p => p.theme.fontSizes.m};
+  font-weight: ${p => p.theme.fontWeights.semiBold};
+  color: ${p => p.theme.colors.whiteColor};
+  text-transform: uppercase;
+
+  text-align: center;
+  background-color: ${p => p.theme.colors.bgColorHeadTable};
+  padding: ${p => p.theme.spacing(2.5)};
+  height: ${p => p.theme.spacing(5)};
+  :not(:last-child) {
+    border-right: ${p => p.theme.borders.normal};
+    border-color: ${p => p.theme.colors.whiteColor};
+  }
+`;
+
+export const BodyRow = styled.tr`
+  :nth-child(even) {
+    background-color: ${p => p.theme.colors.bgTableStat};
+  }
+
+  :nth-child(odd) {
+    background-color: ${p => p.theme.colors.bgColorTableMain};
+  }
+`;
+
+export const BodyCell = styled.td`
+  font-size: ${p => p.theme.fontSizes.s};
+  font-weight: ${p => p.theme.fontWeights.regular};
+  color: ${p => p.theme.colors.secondaryFontColor};
+  :first-child {
+    text-transform: capitalize;
+  }
+
+  min-width: 200px;
+  text-align: center;
+  padding: ${p => p.theme.spacing(2.5)};
+  height: ${p => p.theme.spacing(5)};
+
+  :not(:first-child) {
+    border-left: ${p => p.theme.borders.normal};
+    border-bottom: ${p => p.theme.borders.normal};
+    border-color: ${p => p.theme.colors.borderColor};
+  }
+`;
