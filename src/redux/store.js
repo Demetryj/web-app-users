a2412d963fb1f6647f150b6a76e54bf22b9bac1c
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-20 13:56:41 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import { usersReducer } from './users/usersSlice';
-// import { filterReducer } from './filter/feilterSlice';
+import { configureStore } from '@reduxjs/toolkit';
+import { usersReducer } from './users/usersSlice';
+import { filterReducer } from './filter/feilterSlice';
 
-// export const store = configureStore({
-//   reducer: {
-//     users: usersReducer,
-//     filter: filterReducer,
-//   },
-// });
+export const store = configureStore({
+  reducer: {
+    users: usersReducer,
+    filter: filterReducer,
+  },
+});
