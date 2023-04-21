73acaf535fd704f8d4edf16131c4bc7746973b54
Demetryj <103371743+Demetryj@users.noreply.github.com>
2023-04-21 06:25:02 +0300

    Update Modal.styled.js

--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -4,20 +4,15 @@ export const Overlay = styled.div`
   position: fixed;
   top: 0;
   left: 0;
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  width: 100%;
+  height: 100%;
+  /* padding: ${p => p.theme.spacing(4)}; */
+  /* overflow-y: hidden; */
+  /* overflow-y: scroll; */
+
+  /* display: flex; */
+  /* justify-content: center; */
+  /* align-items: center; */
   background-color: ${props => props.theme.colors.whiteColor};
   z-index: 1200;
-`;
-
-export const WindiwModal = styled.div`
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  /* max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px); */
 `;
